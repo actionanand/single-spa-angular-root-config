f45eb5ee9de4ec2d265e3f7c5f98f31e2c1a7694
Anand raja <meetanandns@gmail.com>
2025-02-22 11:42:44 +0000

    scaffold

---
+++ src/app/microapp/microapp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-microapp',
+  standalone: true,
+  imports: [],
+  templateUrl: './microapp.component.html',
+  styleUrl: './microapp.component.scss',
+})
+export class MicroappComponent {}
