5d44a2e02f4b02e3062db763b029c64a6424196e
Anand raja <meetanandns@gmail.com>
2025-03-06 18:08:47 +0000

    dev url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,20 +1,14 @@
 export const environment = {
   production: false,
   'sspa-apps-map': {
-    'single-spa-angular': 'https://cdn.jsdelivr.net/gh/actionanand/single-spa-angular@gh-pages/main.js',
-    '@actionanand/single-spa-react':
-      'https://cdn.jsdelivr.net/gh/actionanand/single-spa-react@gh-pages/actionanand-single-spa-react.js',
-    '@actionanand/single-spa-vue': 'https://cdn.jsdelivr.net/gh/actionanand/single-spa-vue@gh-pages/js/app.27022a70.js',
-    '@actionanand/single-spa-svelte':
-      'https://cdn.jsdelivr.net/gh/actionanand/single-spa-svelte@gh-pages/actionanand-single-spa-svelte.js',
-    '@actionanand/single-spa-footer':
-      'https://cdn.jsdelivr.net/gh/actionanand/single-spa-footer@gh-pages/actionanand-single-spa-footer.js',
-    '@actionanand/single-spa-404':
-      'https://cdn.jsdelivr.net/gh/actionanand/single-spa-404@gh-pages/actionanand-single-spa-404.js',
-    '@actionanand/single-spa-vanilla-js':
-      'https://cdn.jsdelivr.net/gh/actionanand/single-spa-vanilla-js@gh-pages/index.js',
-    '@actionanand/utility':
-      'https://cdn.jsdelivr.net/gh/actionanand/single-spa-utility@gh-pages/actionanand-utility.js',
-    'single-spa-sidebar': 'https://cdn.jsdelivr.net/gh/actionanand/single-spa-sidebar@gh-pages/main.js',
+    'single-spa-angular': 'http://localhost:20222/main.js',
+    '@actionanand/single-spa-react': 'http://localhost:20223/actionanand-single-spa-react.js',
+    '@actionanand/single-spa-vue': 'http://localhost:20224/js/app.js',
+    '@actionanand/single-spa-svelte': 'http://localhost:20225/actionanand-single-spa-svelte.js',
+    '@actionanand/single-spa-footer': 'http://localhost:20226/actionanand-single-spa-footer.js',
+    '@actionanand/single-spa-404': 'http://localhost:20227/actionanand-single-spa-404.js',
+    '@actionanand/single-spa-vanilla-js': 'http://localhost:20228/index.js',
+    '@actionanand/utility': 'http://localhost:20229/actionanand-utility.js',
+    'single-spa-sidebar': 'http://localhost:20251/main.js',
   } as { [key: string]: string },
 };
